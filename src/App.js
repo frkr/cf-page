948f794f8b5940276584141bea0dd0011a479781
DaviSM <davimesquita@gmail.com>
2023-06-07 10:32:15 -0300

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import './App.css';
 import Task from './task';
 import { Suspense } from 'react';
 
-function App() {
+export default function App() {
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<Suspense fallback={<div>Loading...</div>}>
-					<Task />
-				</Suspense>
-			</header>
+		<div>
+			<Suspense fallback={<div>Loading...</div>}>
+				<Task />
+			</Suspense>
 		</div>
 	);
 }
-
-export default App;
--- src/task/index.js
+++ src/task/index.js
@@ -16,12 +16,35 @@ export default function Task() {
 
 	return (
 		<Fragment>
-			<ui>
-				<li>
-					<button onClick={dothething}>Funciona</button>
-					{teste.text}
-				</li>
-			</ui>
+			<table className='table'>
+				<thead>
+				<tr>
+					<th scope='col'>#</th>
+					<th scope='col'>First</th>
+					<th scope='col'>Last</th>
+					<th scope='col'>Handle</th>
+				</tr>
+				</thead>
+				<tbody>
+				<tr>
+					<th scope='row'>1</th>
+					<td>Mark</td>
+					<td>Otto</td>
+					<td>@mdo</td>
+				</tr>
+				<tr>
+					<th scope='row'>2</th>
+					<td>Jacob</td>
+					<td>Thornton</td>
+					<td>@fat</td>
+				</tr>
+				<tr>
+					<th scope='row'>3</th>
+					<td colSpan='2'>Larry the Bird</td>
+					<td>@twitter</td>
+				</tr>
+				</tbody>
+			</table>
 		</Fragment>
 	);
 
